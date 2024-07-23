66f99973e30fa9ce761c852c534df2f27c84041b
PDBD071 <keli.martins@pdbomdespacho.com.br>
2024-07-23 12:12:11 -0300

    Add files via upload

---
+++ Modulo 3/aula2_entrada_outrobar2.py
@@ -0,0 +1,17 @@
+# Entrada de dados
+# Idade de Juliana
+# Idade de Cris
+idade_juliana = int(input("Digite a idade de Juliana: "))
+idade_cris = int(input("Digite a idade de Cris: "))
+
+# Processamento
+# True se pelo menos um dos dois for maior de idade
+# <exp1> Juliana é maior de idade
+# <exp2> Cris é maior de idade
+# <exp1> and <exp2>
+# Em qualquer outro caso
+pode_entrar =  idade_juliana >= 18 or idade_cris >= 18
+
+# Saída
+print(pode_entrar)
+
