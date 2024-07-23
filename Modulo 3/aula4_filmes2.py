9d38e809ab69c1c9f0f489f0595210b3a796cc6b
PDBD071 <keli.martins@pdbomdespacho.com.br>
2024-07-23 17:18:29 -0300

    Add files via upload

---
+++ Modulo 3/aula4_filmes2.py
@@ -0,0 +1,19 @@
+# Entrada
+avaliacao = int(input("Por favor, insira a avaliação do filme (de 1 a 5): "))
+
+# Processamento
+if avaliacao == 5:
+    print("Excelente!")
+elif avaliacao == 4:
+    print("Muito Bom!")
+elif avaliacao == 3:
+    print("Bom!")
+elif avaliacao == 2:
+    print("Regular.")
+elif avaliacao == 1:
+    print("Ruim.")
+else:
+
+# Saída
+    print("Avaliação inválida. Por favor, insira um número entre 1 e 5.")
+
