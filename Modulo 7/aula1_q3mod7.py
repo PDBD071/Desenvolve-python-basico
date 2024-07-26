982517d5865046692d25c6f88e5952d643466abf
PDBD071 <keli.martins@pdbomdespacho.com.br>
2024-07-26 17:35:00 -0300

    Add files via upload

---
+++ Modulo 7/aula1_q3mod7.py
@@ -0,0 +1,9 @@
+# Solicita uma frase ao usuário
+frase = input("Digite uma frase: ")
+
+# Conta o número de espaços em branco na frase
+numero_de_espacos = frase.count(' ')
+
+# Exibe o número de espaços em branco
+print(f"Número de espaços em branco: {numero_de_espacos}")
+
