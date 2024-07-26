2283ed5335848813ad1a4ed7d6ebb66696a2e49e
PDBD071 <keli.martins@pdbomdespacho.com.br>
2024-07-26 16:55:50 -0300

    Add files via upload

---
+++ Modulo 7/aula1_q2mod7.py
@@ -0,0 +1,9 @@
+# Solicita o primeiro nome e o sobrenome do usuário
+primeiro_nome = input("Digite seu primeiro nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+# Concatena o primeiro nome e o sobrenome
+nome_completo = primeiro_nome + " " + sobrenome
+
+# Exibe a mensagem de boas-vindas
+print(f"Bem-vindo(a), {nome_completo}!")
